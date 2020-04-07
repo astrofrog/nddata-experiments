cefa53afb3b9bed6dfb940d65ad3217c473be3d6
Stuart Mumford <stuart@cadair.com>
2020-04-07 13:49:39 +0100

    old commit

--- decorators.py
+++ decorators.py
@@ -1,9 +1,10 @@
 from functools import wraps
 import inspect
 import numpy as np
+from astropy.nddata import NDData
 
 
-def view_as_nddata(expected_type):
+def view_as_nddata(expected_type=NDData):
     def decorator(func):
         @wraps(func)
         def func_wrapper(*args, **kwargs):
@@ -13,12 +14,14 @@ def view_as_nddata(expected_type):
             # Get the expected data argument
             input_data = args[0]
 
-            new_input_data = expected_type.__from_nddata__(input_data)
+            new_input_data = input_data
+            if expected_type != NDData:
+                new_input_data = expected_type.__from_nddata__(input_data)
 
-            output_data = func(new_input_data, *args[1:], **kwargs)
+            output_data, *returns = func(new_input_data, *args[1:], **kwargs)
 
             new_output_data = input_data.__from_nddata__(output_data, input_data)
 
-            return new_output_data
+            return new_output_data, *returns
         return func_wrapper
     return decorator
